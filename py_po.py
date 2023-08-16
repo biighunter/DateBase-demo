c1a4d902c667cb5c318a3e44993d06abf0a682dd
hu-mamad <muhammad.h.0106@gmail.com>
2023-08-16 22:26:17 +0330

    Update

--- py_po.py
+++ py_po.py
@@ -1,19 +1,10 @@
-import os
-import time
-
-# Database details
-DB_HOST = '146.190.26.15'
-DB_USER = 'biighunter'
-DB_PASS = '1122'
-DB_NAME = 'postgresdb'
-
-# Backup details
-BACKUP_PATH = '/root/DateBase-demo/backup/'
-TIMESTAMP = time.strftime('%Y-%m-%d-%H-%M-%S')
-BACKUP_FILE = DB_NAME + '_' + TIMESTAMP + '.sql'
-
-# Command to take a backup
-BACKUP_CMD = "pg_dump -h {0} -U {1} -d {2} > {3}".format(DB_HOST, DB_USER, DB_NAME, os.path.join(BACKUP_PATH, BACKUP_FILE))
-
-# Execute the backup command
-os.system(BACKUP_CMD)
+import gzip
+import subprocess
+with gzip.open('backup.gz', 'wb') as f:
+  popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+  
+  for stdout_line in iter(popen.stdout.readline, ""):
+    f.write(stdout_line.encode('utf-8'))
+  
+  popen.stdout.close()
+  popen.wait()
