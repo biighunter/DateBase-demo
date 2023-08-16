3afd70248a470c84b22c5f8b14aedef01930debb
hu-mamad <muhammad.h.0106@gmail.com>
2023-08-17 01:22:31 +0330

    Update

--- py_po.py
+++ py_po.py
@@ -1,51 +1,11 @@
-import subprocess
-import datetime
 import psycopg2
 
-# Database connection parameters
-db_params = {
-    "host": "146.190.26.15",
-    "port": "5432",
-    "database": "postgresdb",
-    "user": "biighunter",
-    "password": "1122"
-}
+db_host = "146.190.26.15"
+db_port = "5432"
+db_database = "postgresdb"
+db_user = "biighunter"
+db_pass = "1122"
 
-# Backup settings
-backup_dir = "/root/DateBase-demo/backup"
-backup_filename = f"backup_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.sql"
+conn = psycopg2.connect(dbname=db_database, user=db_user, password=db_pass, host=db_host, port=db_port)
 
-# Create a backup
-def create_backup():
-    try:
-        # Connect to the database
-        conn = psycopg2.connect(**db_params)
-        conn.autocommit = True
-        cursor = conn.cursor()
-
-        # Run pg_dump command to create a backup
-        backup_path = f"{backup_dir}/{backup_filename}"
-        pg_dump_path = "/usr/bin/pg_dump"
-        dump_command = [
-            pg_dump_path,
-            f"--host={db_params['host']}",
-            f"--port={db_params['port']}",
-            f"--username={db_params['user']}",
-            f"--dbname={db_params['database']}",
-            f"--file={backup_path}"
-        ]
-
-        subprocess.run(dump_command, check=True)
-
-        print("Backup created successfully:", backup_path)
-
-    except Exception as e:
-        print("Error creating backup:", e)
-
-    finally:
-        if conn:
-            cursor.close()
-            conn.close()
-
-if __name__ == "__main__":
-    create_backup()
+conn.close()
