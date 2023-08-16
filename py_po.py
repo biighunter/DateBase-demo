05bc08e7843679494f7dbab210479f6d72f8ee7c
hu-mamad <muhammad.h.0106@gmail.com>
2023-08-16 21:49:32 +0330

    Update

--- py_po.py
+++ py_po.py
@@ -4,16 +4,16 @@ import psycopg2
 
 # Database connection parameters
 db_params = {
-    'host': '146.190.26.15',
-    'port': '5432',
-    'database': 'postgresdb',
-    'user': 'biighunter',
-    'password': '1122'
+    "host": "146.190.26.15",
+    "port": "5432",
+    "database": "postgresdb",
+    "user": "biighunter",
+    "password": "1122"
 }
 
 # Backup settings
-backup_dir = '/path/to/backup/directory'
-backup_filename = f"backup_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.sql"
+backup_dir = "/path/to/backup/directory"
+backup_filename = f"backup_{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}.sql"
 
 # Create a backup
 def create_backup():
@@ -26,11 +26,11 @@ def create_backup():
         # Run pg_dump command to create a backup
         backup_path = f"{backup_dir}/{backup_filename}"
         dump_command = [
-            'pg_dump',
-            f"--host={db_params['host']}",
-            f"--port={db_params['port']}",
-            f"--username={db_params['user']}",
-            f"--dbname={db_params['database']}",
+            "pg_dump",
+            f"--host={db_params["host"]}",
+            f"--port={db_params["port"]}",
+            f"--username={db_params["user"]}",
+            f"--dbname={db_params["database"]}",
             f"--file={backup_path}"
         ]
 
@@ -46,5 +46,5 @@ def create_backup():
             cursor.close()
             conn.close()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     create_backup()
